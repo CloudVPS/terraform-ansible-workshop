85cccaf30c751037105bf30fa15d94a7ad27b504
ceesios <cees@cloudvps.com>
2018-08-07 15:53:13 +0200

    updated userdata

--- user_data_workshopserver.sh
+++ user_data_workshopserver.sh
@@ -1,5 +1,8 @@
 #! /bin/sh -v
-apt update; apt install -y git ansible
-cd /etc/ansible/roles
-git clone https://github.com/CloudVPS/ansible-roles.git /etc/ansible/roles/
-git clone https://github.com/CloudVPS/terraform-ansible-workshop.git
+sudo apt update; apt install -y git ansible golang-go zip
+sudo git clone https://github.com/CloudVPS/ansible-roles.git /etc/ansible/roles/
+sudo git clone https://github.com/CloudVPS/terraform-ansible-workshop.git
+sudo wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
+sudo unzip terraform_0.11.7_linux_amd64.zip -d /bin/
+sudo go get github.com/adammck/terraform-inventory
+
