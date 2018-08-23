8d75a30fd5c1ac70db52aa673467b8482d45f2ac
Attila van der Velde <velde.a.van.der@gmail.com>
2018-08-23 14:46:28 +0200

    Update workshop server user data (#4)
    * Workshop server tooling
    
    * Update
    
    * Add editorconfig

--- user_data_workshopserver.sh
+++ user_data_workshopserver.sh
@@ -1,8 +1,49 @@
-#! /bin/sh -v
-sudo apt update; apt install -y git ansible golang-go zip
-sudo git clone https://github.com/CloudVPS/ansible-roles.git /etc/ansible/roles/
-sudo git clone https://github.com/CloudVPS/terraform-ansible-workshop.git
-sudo wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
-sudo unzip terraform_0.11.7_linux_amd64.zip -d /bin/
-sudo go get github.com/adammck/terraform-inventory
+#!/bin/bash
+set -ex
 
+export DEBIAN_FRONTEND=noninteractive
+
+TERRAFORM_VERSION=0.11.7
+TERRAFORM_CHECKSUM=6b8ce67647a59b2a3f70199c304abca0ddec0e49fd060944c26f666298e23418
+
+# Upgrade packages
+apt-get update && \
+    apt-get upgrade -y -o Dpkg::Options::="--force-confold"
+
+# Install packages
+apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common \
+    unzip \
+    wget
+
+# Install Git & Gitflow toolset
+apt-get install -y git-flow
+
+# Install OpenStack client
+apt-get install -y python-openstackclient
+
+# Install Terraform
+wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip && \
+    echo "${TERRAFORM_CHECKSUM}  terraform_${TERRAFORM_VERSION}_linux_amd64.zip" > terraform.checksum && \
+    sha256sum --strict --check terraform.checksum && \
+    unzip -u terraform_${TERRAFORM_VERSION}_linux_amd64.zip -d /usr/local/bin && \
+    rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip terraform.checksum
+
+# Install Ansible
+apt-add-repository -y ppa:ansible/ansible && \
+    apt-get update && \
+    apt-get install -y ansible
+
+# Install Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add - && \
+    add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) edge" && \
+    apt-get update && \
+    apt-get install -y docker-ce && \
+    usermod -aG docker ubuntu || true
+
+# Clean
+# apt-get clean && \
+#     rm -rf /var/lib/apt/lists/* /var/tmp/* /tmp/*
